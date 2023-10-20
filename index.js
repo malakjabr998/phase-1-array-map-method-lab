e57b22c152d6e4da17970b8e3e018dff9966ffed
malak jabr <malakjabr2@gmail.com>
2023-10-20 22:38:44 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+
+const titleCased = tutorials.map(tutorial => {
+  const words = tutorial.split(" ");
+  const titleCasedWords = words.map(word => {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  });
+  return titleCasedWords.join(" ");
+});
+
+console.log(titleCased);
